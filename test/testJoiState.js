1c7855a5be45fd0e77196d45150e4da8b91ce3f9
intertext.no <orstavik77@gmail.com>
2018-01-05 14:14:46 +0100

    making the JoiState class a unit test

---
+++ test/testJoiState.js
@@ -0,0 +1,29 @@
+describe('test of JoiState', function () {
+
+  it("testing simple State functions", function () {
+
+    window.addEventListener("state-changed", function (ev) {
+      expect(ev.detail).to.deep.equal({a: "a string", reducerOne: "reduceData", _computeOne: "a stringreduceData"});
+    });
+
+    const initialState = {
+      a: "a string"
+    };
+    Object.freeze(initialState);
+
+    let state = new JoiState(initialState);
+    state.bindReduce('state-test-one', TestFunctions.reducerOne, true);
+    state.bindCompute("_computeOne", TestFunctions.computeOne, ["a", "reducerOne"]);
+    state.bindObserve(TestFunctions.observeOne, ["_computeOne"]);
+
+    expect(initialState).to.deep.equal({a: "a string"});
+
+    // console.log(initialState);
+    window.dispatchEvent(new CustomEvent("state-test-one", {
+      bubbles: true,
+      composed: true,
+      detail: "reduceData"
+    }));
+
+  });
+});
