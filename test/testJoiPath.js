dd444310e5f7437e842bd5bb08c4af0a479a1560
intertext.no <orstavik77@gmail.com>
2018-01-05 14:05:34 +0100

    making first tests in Mocha and Chai

--- test/testJoiPath.js
+++ test/testJoiPath.js
@@ -1,93 +1,81 @@
-//example start
-let a = {};
-a.child = "hello";
-
-let firstState = a.child;
-let firstPointer = a;
-
-a.child = "goodbye";
-let secondState = a.child;
-let secondPointer = a;
-
-secondState === "goodbye";      //true
-firstState === "hello";         //true
-firstPointer === secondPointer; //true
-a.child === "goodbye";          //true
-
-a === a; //true
-
 //  problem with mutability occurs with a) big, distributed, many different files all working against the same data
 //  problem with mutability occurs with b) async, different parts of app need to wait for other parts of app to both write to the same properties.
-//example end
+describe('test of immutable function', function () {
 
-//example immutable using JoiPath
-{
-  let a = {};
-  a.child = "hello";
+  it("normal, mutable property setter ", function () {
+    let a = {};
+    a.child = "hello";
+    let firstState = a.child;
+    let firstPointer = a;
 
-  let firstState = a.child;
-  let firstPointer = a;
+    a.child = "goodbye";
+    let secondState = a.child;
+    let secondPointer = a;
 
-  let b = JoiPath.setIn(a, ["child"], "goodbye");
-  let secondState = b.child;
-  let secondPointer = b;
+    expect(secondState).to.be.equal("goodbye");
+    expect(firstState).to.be.equal("hello");
+    expect(firstPointer).to.be.equal(secondPointer);
+    expect(a.child).to.be.equal("goodbye");
+    expect(a).to.be.equal(a);
+  });
 
-  secondState === "goodbye";      //true
-  firstState === "hello";         //true
-  firstPointer !== secondPointer; //true
-  a.child === "hello";          //true
-  b.child === "goodbye";          //true
+  it("JoiPath.setIn, changing child property", function () {
+    let a = {};
+    a.child = "hello";
+    let firstState = a.child;
+    let firstPointer = a;
+    let b = JoiPath.setIn(a, ["child"], "goodbye");
+    let secondState = b.child;
+    let secondPointer = b;
 
-  a !== b; //true
-}
-//example end
+    expect(secondState).to.be.equal("goodbye");
+    expect(firstState).to.be.equal("hello");
+    expect(firstPointer).to.not.equal(secondPointer);
+    expect(a.child).to.be.equal("hello");
+    expect(b.child).to.be.equal("goodbye");
+    expect(a).to.not.equal(b);
+  });
 
-//example immutable using JoiPath with same value
-{
-  let a = {};
-  a.child = "hello";
+  it('JoiPath.setIn, making no changes to child', function () {
+    let a = {};
+    a.child = "hello";
 
-  let firstState = a.child;
-  let firstPointer = a;
+    let firstState = a.child;
+    let firstPointer = a;
 
-  let b = JoiPath.setIn(a, ["child"], "hello");
-  let secondState = b.child;
-  let secondPointer = b;
+    let b = JoiPath.setIn(a, ["child"], "hello");
+    let secondState = b.child;
+    let secondPointer = b;
 
-  secondState === "hello";      //true
-  firstState === "hello";         //true
-  firstPointer === secondPointer; //true
-  a.child === "hello";          //true
-  b.child === "hello";          //true
+    expect(secondState).to.be.equal("hello");
+    expect(firstState).to.be.equal("hello");
+    expect(firstPointer).to.be.equal(secondPointer);
+    expect(a.child).to.be.equal("hello");
+    expect(b.child).to.be.equal("hello");
+    expect(a).to.be.equal(b);
+  });
 
-  a === b; //true
-}
-//example end
+  it('JoiPath.setIn, sibling remain the same object', function () {
+    let a = {};
+    a.child1 = {};
+    a.child2 = {};
+    a.child1.msg = "yes";
+    a.child2.msg = "da";
 
-//example immutable using JoiPath with branches
-{
-  let a = {};
-  a.child1 = {};
-  a.child2 = {};
-  a.child1.msg = "yes";
-  a.child2.msg = "da";
+    let firstPointer = a;
+    let firstPointerChild1 = a.child1;
+    let firstPointerChild2 = a.child2;
 
-  let firstPointer = a;
-  let firstPointerChild1 = a.child1;
-  let firstPointerChild2 = a.child2;
+    let b = JoiPath.setIn(a, ["child1", "msg"], "ja");
 
-  let b = JoiPath.setIn(a, ["child1", "msg"], "ja");
+    let secondPointer = b;
+    let secondPointerChild1 = b.child1;
+    let secondPointerChild2 = b.child2;
 
-  let secondPointer = b;
-  let secondPointerChild1 = b.child1;
-  let secondPointerChild2 = b.child2;
-
-  firstPointer !== secondPointer; //true
-  firstPointerChild1 !== secondPointerChild1; //true
-  firstPointerChild2 === secondPointerChild2; //true
-
-  a.child1 !== b.child1
-  a.child2 === b.child2
-
-}
-//example end
+    expect(firstPointer).to.not.equal(secondPointer);
+    expect(firstPointerChild1).to.not.equal(secondPointerChild1);
+    expect(firstPointerChild2).to.be.equal(secondPointerChild2);
+    expect(a.child1).to.not.equal(b.child1);
+    expect(a.child2).to.be.equal(b.child2);
+  });
+});
