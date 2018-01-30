969222afafa1e0cad5367e9fee35f7dcddd304cf
intertext.no <orstavik77@gmail.com>
2018-01-30 14:06:23 +0100

    added performance.timeOrigin. removed the que from JoiState.

--- src/JoiHistory.js
+++ src/JoiHistory.js
@@ -6,17 +6,17 @@ class JoiHistory {
     //this object will fireAndSetGlobalVariable its history when queried.
   }
 
-  addToHistory(error, startState, reducedState, computedState, state, task, computer, observer, start, que) {
-    const snapShot = JoiHistory._takeSnapshot(error, startState, reducedState, computedState, state, task, computer, observer, start, que);
+  addToHistory(error, startState, reducedState, computedState, state, task, computer, observer, que) {
+    const snapShot = JoiHistory._takeSnapshot(error, startState, reducedState, computedState, state, task, computer, observer, que);
     this.history = [snapShot].concat(this.history);
     // if (this.history.length > 100) this.history = this.history.slice(0,50);
     JoiState.emit("state-history-changed", this.history);
   }
 
-  static _takeSnapshot(error, startState, reducedState, computedState, newState, task, computerInfo, observerInfo, start, que) {
+  static _takeSnapshot(error, startState, reducedState, computedState, newState, task, computerInfo, observerInfo, que) {
     task.taskName = task.reducer.name;
     task.event = {type: task.event.type, detail: task.event.detail};
-    task.start = start;
+    task.timeOrigin = performance.timeOrigin;
     task.stop = performance.now();
     return {
       error,
--- src/JoiState.js
+++ src/JoiState.js
@@ -5,12 +5,11 @@ class JoiState {
     this.observer = new JoiCompute(0);
     this.state = JoiGraph.deepFreeze(initial || {});
     this.que = [];
-
     this.history = new JoiHistory();
   }
 
-  bindReduce(eventName, reducer, throttle = true) {
-    window.addEventListener(eventName, e => this._runOrAddToQue(e, reducer, throttle));
+  bindReduce(eventName, reducer) {
+    window.addEventListener(eventName, event => this._run({event, reducer, start: performance.now()}));
   }
 
   bindCompute(returnProp, computeFunc, argsAsStrings) {
@@ -21,20 +20,7 @@ class JoiState {
     this.observer.bind(observeFunc, argsAsStrings);
   }
 
-  //todo make a test for this in an async multithreaded environment
-  //todo this que is untested and unsafe in a multithreaded environment..
-  _runOrAddToQue(e, reducer, throttle) {
-    //first, I add to/update the que items. This i always do.
-    this.que = JoiState._updateQueOrAddToTheEndOfQue(this.que, reducer, e, throttle);
-    //second, I check if I am the thread that should run this que, or if another thread is already running on this que.
-    //now, i do this by checking if the que.length is bigger than 0. This will not work. This can cause 2 threads to start working on it.
-    //maybe.. if you have web workers.. can that ever be the case?? can i ever get the reducer to be triggered from 2 places at the same time??
-    while (this.que.length > 0)
-      this._reduceComputeObserveInner(this.que.shift());      // if (this.que.length > 100) setTimeout(()=> this._reduceComputeObserveInner(this.que[0]), 0);
-  }
-
-  _reduceComputeObserveInner(task) {
-    let start = performance.now();
+  _run(task) {
     let startState = this.state;
     let reducedState = task.reducer(startState, task.event.detail);         //1. reduce
     let computedState, error;
@@ -50,30 +36,10 @@ class JoiState {
         JoiState.emit("state-error", error);
       }
     }
-    this.history.addToHistory(error, startState, reducedState, computedState, this.state, task, this.computer, this.observer, start, this.que);
+    this.history.addToHistory(error, startState, reducedState, computedState, this.state, task, this.computer, this.observer, this.que);
   }
 
-  //todo this now mutates the que. I think this is the right approach, but im not sure
-  static _updateQueOrAddToTheEndOfQue(que, reducer, event, throttle) {
-    const task = {event, reducer, added: new Date().getTime()};
-    if (throttle) {
-      let i = que.findIndex(existingTask => existingTask.reducer === reducer);
-      if (i >= 0){
-        task.overWritesTask = que[i];
-        que[i] = task;
-        return que;
-      }
-    }
-    que.push(task);
-    return que;
-  }
-
-  static emit(name, payload) {
-    return window.dispatchEvent(new CustomEvent(name, {
-      composed: true,
-      bubbles: true,
-      detail: payload,
-    }));
+  static emit(name, detail) {
+    return window.dispatchEvent(new CustomEvent(name, {composed: true, bubbles: true, detail: detail}));
   }
 }
-
