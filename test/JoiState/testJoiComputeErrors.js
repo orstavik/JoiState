117dae4afc1c8758992ebae9fa2cb8fc4b361662
intertext.no <orstavik77@gmail.com>
2018-01-30 21:38:59 +0100

    added test for working after Error

--- test/JoiState/testJoiComputeErrors.js
+++ test/JoiState/testJoiComputeErrors.js
@@ -101,54 +101,68 @@ const expectedErrorMsg = `Error: StackOverFlowError in JoiCompute (JoiState), pr
 [_c = sum(a, _b)]
 [_b = sum(a, _c)]`;
 
-describe('Infinite loop and NaN', function () {
+describe('Error handling (ATT!! These tests fail due to CORS policy if they are not run on a server)', function () {
 
-  it("the engine understands that a value changing from NaN to NaN is not a change", function (done) {
-    const reducerEventName = 'state-test-infinte2';
-    const startState = {
-      a: 1
-    };
-    const state = new JoiState(startState);
-
+  it("JoiState simple error", function (done) {
     const reducerOne = function (state, detail) {
       return JoiGraph.setIn(state, "a", detail);
     };
-    const sum = function (a, b) {
-      return a + b;                //this returns NaN when b is not a number, which will give a different error
+    const reducerFail = function (state) {
+      throw new Error("i should fail");
     };
-    state.bindReduce(reducerEventName, reducerOne, true);
-    state.bindCompute("_b", sum, ["a", "_c"]);   //infinite loop, when _b is updated,
-    state.bindCompute("_c", sum, ["a", "_b"]);   // _c will need to be recalculated, and that triggers update of _b again
-    state.bindOnEnd(newState => {
-      expect(newState.a).to.be.equal(2);
-      expect(newState._b).to.be.NaN;
-      expect(newState._c).to.be.NaN;
+    const state = new JoiState();
+    state.bindReduce('state-test-fail', reducerFail, true);
+    //the error is thrown out as a global error, so you need to capture
+    window.onerror = function (a, b, c, d, error) {
+      expect("Error: i should fail").to.be.equal(error.toString());
+      state.detachReducers();
       done();
-    });
-    window.dispatchEvent(new CustomEvent(reducerEventName, {bubbles: true, composed: true, detail: 2}));
+    };
+    window.dispatchEvent(new CustomEvent('state-test-fail', {bubbles: true, composed: true}));
   });
 
+  it("JoiState still works after error", function (done) {
+    const reducerOne = function (state, detail) {
+      return JoiGraph.setIn(state, "a", detail);
+    };
+    const reducerFail = function (state, detail) {
+      throw new Error("i should fail");
+    };
+    const state = new JoiState({a: 1});
+    state.bindReduce('state-test-fail', reducerFail, true);
+    state.bindReduce('state-test-working', reducerOne, true);
+    //the error is thrown out as a global error, so you need to capture it on window
+    window.onerror = function (a, b, c, d, error) {
+      expect("Error: i should fail").to.be.equal(error.toString());
+    };
+    state.bindOnEnd(function (newState) {
+      expect(newState.a).to.be.equal(3);
+      state.detachReducers();
+      done();
+    });
+    window.dispatchEvent(new CustomEvent('state-test-fail', {bubbles: true, composed: true, detail: null}));
+    window.dispatchEvent(new CustomEvent('state-test-working', {bubbles: true, composed: true, detail: 3}));
+  });
 
-  it("make sure an infinite loop happens (ATT!! This function fails due to CORS policy if the tests are not run on a server)", function (done) {
-
+  it("make sure an infinite loop happens", function (done) {
     const reducerOne = function (state, detail) {
       return JoiGraph.setIn(state, "a", detail);
     };
     const sum = function (a, b) {
       return (a || 0) + (b || 0);
     };
-    const state = new JoiState({
-      a: 1
-    });
+    const state = new JoiState({a: 1});
     state.bindReduce('state-test-infinte', reducerOne, true);
     state.bindCompute("_b", sum, ["a", "_c"]);   //infinite loop, when _b is updated,
     state.bindCompute("_c", sum, ["a", "_b"]);   // _c will need to be recalculated, and that triggers update of _b again
     //the error is thrown out as a global error, so you need to capture
-    window.onerror = function (a,b,c,d,error) {
+    window.onerror = function (a, b, c, d, error) {
       expect(expectedErrorMsg).to.be.equal(error.toString());
+      state.detachReducers();
       done();
     };
     window.dispatchEvent(new CustomEvent('state-test-infinte', {bubbles: true, composed: true, detail: 2}));
   });
+
 });
 
